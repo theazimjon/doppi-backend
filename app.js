ab57cd68881e1dbb3e7e4de3ca08d602bf54eb2e
Azimjon Umarov <94437438+theazimjon@users.noreply.github.com>
2022-10-22 23:00:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin : '*', // !!! dev mode
+    origin : ['*', "https://doppi-frontend.vercel.app"], // !!! dev mode
     credentials:true
 }));
 app.use(express.urlencoded({
